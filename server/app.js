31e5421bc550b79a60d8d2744367be01dace4a0d
presia27 <pre.sia977@outlook.com>
2025-03-19 22:27:05 -0700

    Feature - Implement basic parts of get by ID API

--- server/app.js
+++ server/app.js
@@ -10,15 +10,32 @@ const app = express();
 /* Routes */
 app.get('/get-bridges', async (req, res) => {
     const queryCols = "id, name, region"; // columns to use for query
-    let bridgeData = await getAllBridges(queryCols);
+    let bridgeData = await getBridges(queryCols, -1);
+    let bridgeWrap = {"bridges": bridgeData};
+    res.send(bridgeWrap);
+});
+
+app.get('/get-bridge-by-id', async (req, res) => {
+    const searchMethod = "id";
+    const requestedId = req.query.id;
+    const queryCols = "*"; // query all
+    let bridgeData = await getBridges(queryCols, requestedId, searchMethod);
     res.send(bridgeData);
 });
 
 
 /* Helper functions */
-const getAllBridges = async (queryCols) => {
+// Set bridgeId to -1 for all bridges
+// Method refers to the search parameter (e.g. search by id, name, etc.)
+const getBridges = async (queryCols, bridgeId, method) => {
+    let query = `SELECT ${queryCols} FROM bridges`;
+    if ((bridgeId != null && bridgeId != undefined && bridgeId != -1)
+            && (method != null && method != undefined)) {
+        query += ` WHERE ${method}='${bridgeId}'`;
+    }
+
     const result = await connector.query(
-        `SELECT ${queryCols} FROM bridges`
+        query
     );
     //console.log(result.rows);
     return result.rows;
