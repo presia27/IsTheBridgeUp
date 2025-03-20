9aa22ce49b59c938466c34d041e9d14a7ba96d6a
presia27 <pre.sia977@outlook.com>
2025-03-19 21:55:28 -0700

    Feature - Implement bridge list API call

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config();
+const connector = require('./dbconfig/pgconnector');
 
 /* Initialize Express */
 const app = express();
@@ -7,12 +8,24 @@ const app = express();
 /* Middleware */
 
 /* Routes */
-app.get('/get-bridges', (req, res) => {
-    res.send('Repsonse');
+app.get('/get-bridges', async (req, res) => {
+    const queryCols = "id, name, region"; // columns to use for query
+    let bridgeData = await getAllBridges(queryCols);
+    res.send(bridgeData);
 });
+
+
+/* Helper functions */
+const getAllBridges = async (queryCols) => {
+    const result = await connector.query(
+        `SELECT ${queryCols} FROM bridges`
+    );
+    //console.log(result.rows);
+    return result.rows;
+}
 
 /* Configure port */
 const port = process.env.SERVER_PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`);
 });
