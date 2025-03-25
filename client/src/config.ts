5039120c495f38fb98e3b39996b574c6c04bfe7c
presia27 <pre.sia977@outlook.com>
2025-03-24 23:25:03 -0700

    Feature - change backend URL to live service

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,3 @@
 /* CONFIGURATION FILE */
-export const API_BASEURL = "http://localhost:3000/";
+//export const API_BASEURL = "http://localhost:3000/";
+export const API_BASEURL = "https://isthebridgeup-api.vercel.app";
