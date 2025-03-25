cb32e8a3b33ba5e9c4c3afb1002a7afcd1f811c7
presia27 <pre.sia977@outlook.com>
2025-03-24 22:39:49 -0700

    Fix - Change default port to 3000 (React+Vite uses 5173)

--- server/app.js
+++ server/app.js
@@ -167,7 +167,7 @@ const appendTimeTags = (theData) => {
 
 
 /* Configure port */
-const port = process.env.SERVER_PORT || 3001;
+const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
