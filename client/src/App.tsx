49a55313b658ae15373965e969686f31cd5419a5
presia27 <pre.sia977@outlook.com>
2025-03-26 20:45:58 -0700

    Feature - Create contexts for API and dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import { DarkModeProvider } from './context/DarkModeContext'; // Shared context for dark mode
+import { BridgeProvider } from './context/BridgeContext';
 import PageLayout from "./pages/Layout";
 import HomePage from "./pages/Home";
 import About from './pages/About';
@@ -10,14 +12,19 @@ import APIPage from './pages/API';
 const App: React.FC = () => {
     return (
         <BrowserRouter>
-            <PageLayout>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/faq" element={<FAQ />} />
-                    <Route path="/api" element={<APIPage />} />
-                </Routes>
-            </PageLayout>
+            <DarkModeProvider>
+                <BridgeProvider>
+                    <PageLayout>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/faq" element={<FAQ />} />
+                            <Route path="/api" element={<APIPage />} />
+                        </Routes>
+                    </PageLayout>
+                </BridgeProvider>
+            </DarkModeProvider>
+            
         </BrowserRouter>
     )
 }
