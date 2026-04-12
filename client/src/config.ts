69e85689102679c91e78165e2f5ec75043f1c97f
presia27 <pre.sia977@outlook.com>
2026-04-12 00:29:27 -0700

    Remove server directory, leaving only the client, and changed the API URL

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,3 @@
 /* CONFIGURATION FILE */
 //export const API_BASEURL = "http://localhost:8000/";
-export const API_BASEURL = "https://isthebridgeup-api.vercel.app/";
+export const API_BASEURL = "https://isthebridgeup-api-94f7c293375b.herokuapp.com/";
